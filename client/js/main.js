024c30c0f85f162954d1a9dcd58294c128c81b59
Scott Williams <Scott Williams>
2017-05-16 22:30:09 -0400

    Tested jquery and added Milligram UI framework

---
+++ client/js/main.js
@@ -0,0 +1,7 @@
+//this works, proof that jquery is alive and well on our site
+$('document').ready(function() {
+   //once the page loads, wait 3 seconds, then show an alert!
+    setTimeout(function() {
+        alert("It has been 3 seconds!");
+    }, 3000);
+});
