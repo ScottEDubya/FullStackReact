6e255cce42535b763fa9e80221417888e8544402
Scott Williams <Scott Williams>
2017-05-18 22:58:13 -0400

    Testing the pre-commit steps rd2

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,6 @@
 //this works, proof that jquery is alive and well on our site
 $('document').ready(function() {
-   //once the page loads, wait 3 seconds, then show an alert!
+    //once the page loads, wait 3 seconds, then show an alert!
     setTimeout(function() {
         alert("It has been 3 seconds!");
     }, 3000);
