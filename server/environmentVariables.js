bd46a01b5215196c02b045ad609e7ed29a096d3b
Scott Williams <sew62@zips.uakron.edu>
2017-11-20 23:04:41 -0500

    updated to better named db

--- server/environmentVariables.js
+++ server/environmentVariables.js
@@ -1,4 +1,4 @@
 module.exports = {
     MONGO_HOST: process.env.mongoHost || "localhost",
-    MONGO_DB_NAME: process.env.mongoDbName || "scottsdb"
+    MONGO_DB_NAME: process.env.mongoDbName || "fsrdb"
 };
